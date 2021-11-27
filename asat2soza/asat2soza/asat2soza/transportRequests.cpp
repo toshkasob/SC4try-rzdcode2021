8c90f6a316591440bdd7561c614e3e94ce5401b9
toshkasob <toshka_sob@mail.ru>
2021-11-27 20:44:02 +0300

    add COPY constructor

--- asat2soza/asat2soza/asat2soza/transportRequests.cpp
+++ asat2soza/asat2soza/asat2soza/transportRequests.cpp
@@ -15,8 +15,9 @@ TransporRequestsList::readFileWithTabDelim(const std::string & filename)
         OK, ERROR_FILE_OPEN
     };
 
-    FILE* file4read = fopen(filename.c_str(), "r");
-    std::fstream finCsv(file4read);
+    //FILE* file4read = fopen(filename.c_str(), "r");
+    //std::fstream finCsv(file4read);
+    std::fstream finCsv(filename.c_str());
     if (!finCsv.is_open())
         return ERROR_FILE_OPEN;
     std::string str_str;
@@ -37,171 +38,175 @@ TransportRequestUnitFull::parseRequest(const std::string & str_str)
 {
     enum returnErrors
     {
-        OK, OTHER_ERROR
+        OK, STR_IS_NULL
     };
+    if (str_str == "")
+        return STR_IS_NULL;
 
-        size_t prev_pos = -1;
-        size_t next_pos = -1;
-        size_t delta_pos = 0;
-        std::string delim = "\t";
-        std::string cur_cell = "";
+    size_t prev_pos = -1;
+    size_t next_pos = -1;
+    size_t delta_pos = 0;
+    std::string delim = "\t";
+    std::string cur_cell = "";
 
-        delta_pos = delim.length();
+    delta_pos = delim.length();
 
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        this->request_numb_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    this->request_numb_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
 
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        this->request_id_intro_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    this->request_id_intro_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
 
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        this->status_description_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    this->status_description_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
 
-        // date get vehicle
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
-        {
-            size_t prev4date = -1;
-            size_t next4date = -1;
-            size_t delta4date = 0;
-            std::string str4date = "";
+    // date get vehicle
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
+    {
+        size_t prev4date = -1;
+        size_t next4date = -1;
+        size_t delta4date = 0;
+        std::string str4date = "";
 
-            std::string delim4date = "/";
-            if (cur_cell.find(delim4date) == std::string::npos)
-                delim4date = ".";
-            delta4date = delim4date.length();
+        std::string delim4date = "/";
+        if (cur_cell.find(delim4date) == std::string::npos)
+            delim4date = ".";
+        delta4date = delim4date.length();
 
-            // day
-            prev4date = next4date + delta4date;
-            next4date = cur_cell.find(delim4date, prev4date);
-            (next4date == std::string::npos && prev4date != std::string::npos) ? (next4date = cur_cell.length()) : NULL ;
-            str4date = (next4date != std::string::npos && prev4date != std::string::npos) ? 
-                       cur_cell.substr(prev4date, next4date - prev4date) : "01";
-            //auto tmp = std::strtol(str4date.c_str(), NULL, str4date.length());
-            std::istringstream (str4date) >> this->time_4_get_vehicle_.tm_mday ;
+        // day
+        prev4date = next4date + delta4date;
+        next4date = cur_cell.find(delim4date, prev4date);
+        (next4date == std::string::npos && prev4date != std::string::npos) ? (next4date = cur_cell.length()) : NULL ;
+        str4date = (next4date != std::string::npos && prev4date != std::string::npos) ? 
+                    cur_cell.substr(prev4date, next4date - prev4date) : "01";
+        //auto tmp = std::strtol(str4date.c_str(), NULL, str4date.length());
+        std::istringstream (str4date) >> this->time_4_get_vehicle_.tm_mday ;
 
-            // month
-            prev4date = next4date + delta4date;
-            next4date = cur_cell.find(delim4date, prev4date);
-            (next4date == std::string::npos && prev4date != std::string::npos) ? (next4date = cur_cell.length()) : NULL;
-            str4date = (next4date != std::string::npos && prev4date != std::string::npos) ?
-                cur_cell.substr(prev4date, next4date - prev4date) : "01";
-            std::istringstream(str4date) >> this->time_4_get_vehicle_.tm_mon;
+        // month
+        prev4date = next4date + delta4date;
+        next4date = cur_cell.find(delim4date, prev4date);
+        (next4date == std::string::npos && prev4date != std::string::npos) ? (next4date = cur_cell.length()) : NULL;
+        str4date = (next4date != std::string::npos && prev4date != std::string::npos) ?
+            cur_cell.substr(prev4date, next4date - prev4date) : "01";
+        std::istringstream(str4date) >> this->time_4_get_vehicle_.tm_mon;
 
-            // year
-            prev4date = next4date + delta4date;
-            next4date = cur_cell.find(delim4date, prev4date);
-            (next4date == std::string::npos && prev4date != std::string::npos) ? (next4date = cur_cell.length()) : NULL;
-            str4date = (next4date != std::string::npos && prev4date != std::string::npos) ?
-                cur_cell.substr(prev4date, next4date - prev4date) : "2000";
-            std::istringstream(str4date) >> this->time_4_get_vehicle_.tm_year;
+        // year
+        prev4date = next4date + delta4date;
+        next4date = cur_cell.find(delim4date, prev4date);
+        (next4date == std::string::npos && prev4date != std::string::npos) ? (next4date = cur_cell.length()) : NULL;
+        str4date = (next4date != std::string::npos && prev4date != std::string::npos) ?
+            cur_cell.substr(prev4date, next4date - prev4date) : "2000";
+        std::istringstream(str4date) >> this->time_4_get_vehicle_.tm_year;
+    }
+
+    // time get vehicle
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
+    {
+        size_t prev4time = -1;
+        size_t next4time = -1;
+        size_t delta4time = 0;
+        std::string str4time = "";
+
+        std::string delim4time = ":";
+        if (cur_cell.find(delim4time) == std::string::npos)
+            delim4time = "-";
+        delta4time = delim4time.length();
+
+        // hour
+        prev4time = next4time + delta4time;
+        next4time = cur_cell.find(delim4time, prev4time);
+        (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
+        str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
+            cur_cell.substr(prev4time, next4time - prev4time) : "00";
+        std::istringstream(str4time) >> this->time_4_get_vehicle_.tm_hour;
+        if (cur_cell.find("PM") != std::string::npos) {
+            this->time_4_get_vehicle_.tm_hour += 12;
         }
 
-        // time get vehicle
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
-        {
-            size_t prev4time = -1;
-            size_t next4time = -1;
-            size_t delta4time = 0;
-            std::string str4time = "";
+        // minute
+        prev4time = next4time + delta4time;
+        next4time = cur_cell.find(delim4time, prev4time);
+        (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
+        str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
+            cur_cell.substr(prev4time, next4time - prev4time) : "00";
+        std::istringstream(str4time) >> this->time_4_get_vehicle_.tm_min;
 
-            std::string delim4time = ":";
-            if (cur_cell.find(delim4time) == std::string::npos)
-                delim4time = "-";
-            delta4time = delim4time.length();
+        // sec
+        prev4time = next4time + delta4time;
+        next4time = cur_cell.find(delim4time, prev4time);
+        (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
+        str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
+            cur_cell.substr(prev4time, next4time - prev4time) : "00";
+        std::istringstream(str4time) >> this->time_4_get_vehicle_.tm_sec;
+    }
 
-            // hour
-            prev4time = next4time + delta4time;
-            next4time = cur_cell.find(delim4time, prev4time);
-            (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
-            str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
-                cur_cell.substr(prev4time, next4time - prev4time) : "00";
-            std::istringstream(str4time) >> this->time_4_get_vehicle_.tm_hour;
-            if (cur_cell.find("PM") != std::string::npos) {
-                this->time_4_get_vehicle_.tm_hour += 12;
-            }
+    //passenger
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    (next_pos == std::string::npos && prev_pos != std::string::npos) ? (next_pos = str_str.length()) : NULL;
+    cur_cell = (next_pos != std::string::npos && prev_pos != std::string::npos) ?
+        str_str.substr(prev_pos, next_pos - prev_pos) : "0";
+    std::istringstream(cur_cell) >> this->passengers_numb_;
 
-            // minute
-            prev4time = next4time + delta4time;
-            next4time = cur_cell.find(delim4time, prev4time);
-            (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
-            str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
-                cur_cell.substr(prev4time, next4time - prev4time) : "00";
-            std::istringstream(str4time) >> this->time_4_get_vehicle_.tm_min;
+    // time return vehicle
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
+    {
+        size_t prev4time = -1;
+        size_t next4time = -1;
+        size_t delta4time = 0;
+        std::string str4time = "";
 
-            // sec
-            prev4time = next4time + delta4time;
-            next4time = cur_cell.find(delim4time, prev4time);
-            (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
-            str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
-                cur_cell.substr(prev4time, next4time - prev4time) : "00";
-            std::istringstream(str4time) >> this->time_4_get_vehicle_.tm_sec;
+        std::string delim4time = ":";
+        if (cur_cell.find(delim4time) == std::string::npos)
+            delim4time = "-";
+        delta4time = delim4time.length();
+
+        // hour
+        prev4time = next4time + delta4time;
+        next4time = cur_cell.find(delim4time, prev4time);
+        (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
+        str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
+            cur_cell.substr(prev4time, next4time - prev4time) : "00";
+        std::istringstream(str4time) >> this->time_return_vehicle_.tm_hour;
+        if (cur_cell.find("PM") != std::string::npos) {
+            this->time_return_vehicle_.tm_hour += 12;
         }
 
-        //passenger
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
-        std::istringstream(cur_cell) >> this->passengers_numb_;
+        // minute
+        prev4time = next4time + delta4time;
+        next4time = cur_cell.find(delim4time, prev4time);
+        (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
+        str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
+            cur_cell.substr(prev4time, next4time - prev4time) : "00";
+        std::istringstream(str4time) >> this->time_return_vehicle_.tm_min;
 
-        // time return vehicle
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
-        {
-            size_t prev4time = -1;
-            size_t next4time = -1;
-            size_t delta4time = 0;
-            std::string str4time = "";
+        // sec
+        prev4time = next4time + delta4time;
+        next4time = cur_cell.find(delim4time, prev4time);
+        (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
+        str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
+            cur_cell.substr(prev4time, next4time - prev4time) : "00";
+        std::istringstream(str4time) >> this->time_return_vehicle_.tm_sec;
+    }
 
-            std::string delim4time = ":";
-            if (cur_cell.find(delim4time) == std::string::npos)
-                delim4time = "-";
-            delta4time = delim4time.length();
+    //pass
 
-            // hour
-            prev4time = next4time + delta4time;
-            next4time = cur_cell.find(delim4time, prev4time);
-            (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
-            str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
-                cur_cell.substr(prev4time, next4time - prev4time) : "00";
-            std::istringstream(str4time) >> this->time_return_vehicle_.tm_hour;
-            if (cur_cell.find("PM") != std::string::npos) {
-                this->time_return_vehicle_.tm_hour += 12;
-            }
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    this->request_numb_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
 
-            // minute
-            prev4time = next4time + delta4time;
-            next4time = cur_cell.find(delim4time, prev4time);
-            (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
-            str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
-                cur_cell.substr(prev4time, next4time - prev4time) : "00";
-            std::istringstream(str4time) >> this->time_return_vehicle_.tm_min;
+    prev_pos = next_pos + delta_pos;
+    next_pos = str_str.find(delim, prev_pos);
+    this->request_numb_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
 
-            // sec
-            prev4time = next4time + delta4time;
-            next4time = cur_cell.find(delim4time, prev4time);
-            (next4time == std::string::npos && prev4time != std::string::npos) ? (next4time = cur_cell.length()) : NULL;
-            str4time = (next4time != std::string::npos && prev4time != std::string::npos) ?
-                cur_cell.substr(prev4time, next4time - prev4time) : "00";
-            std::istringstream(str4time) >> this->time_return_vehicle_.tm_sec;
-        }
-
-        //pass
-
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        this->request_numb_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
-
-        prev_pos = next_pos + delta_pos;
-        next_pos = str_str.find(delim, prev_pos);
-        this->request_numb_ = cur_cell = str_str.substr(prev_pos, next_pos - prev_pos);
-
-        return OK;
+    return OK;
 }
--- asat2soza/asat2soza/asat2soza/transportRequests.h
+++ asat2soza/asat2soza/asat2soza/transportRequests.h
@@ -23,6 +23,16 @@ public:
         //this->time_4_get_vehicle_ = struct tm() ;
     }
 
+    TransportRequestUnit(const TransportRequestUnit& _Src_Request) {
+        this->time_4_get_vehicle_ = _Src_Request.time_4_get_vehicle_; //! Дата и время подачи ТС
+        this->passengers_numb_ = _Src_Request.passengers_numb_;    //! Количество пассажиров
+        this->cargo_volume_ = _Src_Request.cargo_volume_; //! Объем груза: undefined => 0.0
+        this->department_ = _Src_Request.department_; //! Наименование структурного подразделения
+        this->route_ = _Src_Request.route_;  //! Маршрут
+        this->adress_4_get_vehicle_ = _Src_Request.adress_4_get_vehicle_;   //! Адрес подачи ТС
+        this->category_vehicle_str_ = _Src_Request.category_vehicle_str_;   //! Категория ТС(текст)
+    }
+
     TransportRequestUnit(const time_t &      _time_4_get_vehicle,
                          const unsigned int  _passengers_numb = 0,
                          const float         _cargo_volume_ = 0,
@@ -33,6 +43,22 @@ public:
                         ) {
         
     }
+
+    TransportRequestUnit& operator=(const TransportRequestUnit& _Src_Request) 
+    {
+        if (this == &_Src_Request)
+            return *this;
+
+        this->time_4_get_vehicle_ = _Src_Request.time_4_get_vehicle_; //! Дата и время подачи ТС
+        this->passengers_numb_ = _Src_Request.passengers_numb_;    //! Количество пассажиров
+        this->cargo_volume_ = _Src_Request.cargo_volume_; //! Объем груза: undefined => 0.0
+        this->department_ = _Src_Request.department_; //! Наименование структурного подразделения
+        this->route_ = _Src_Request.route_;  //! Маршрут
+        this->adress_4_get_vehicle_ = _Src_Request.adress_4_get_vehicle_;   //! Адрес подачи ТС
+        this->category_vehicle_str_ = _Src_Request.category_vehicle_str_;   //! Категория ТС(текст)
+        return *this;
+    }
+
 };
 
 /*! Заявка на автотранспорт (выгрузка АСАТ)
@@ -84,8 +110,105 @@ public:
     std::string    motor_depot_str_;    //! Наименование автобазы/автоколонны
     bool           is_request_cancel_by_deadline_;  //! Заявка отменена с нарушением сроков
 
-    TransportRequestUnitFull() {
+    //TransportRequestUnitFull(TransportRequestUnitFull & _Src_Request) {
+    //    
+    //}
 
+    TransportRequestUnitFull(TransportRequestUnitFull & _Src_Request) {
+        this->request_numb_ = _Src_Request.request_numb_;   //! Номер заявки
+        this->request_id_intro_ = _Src_Request.request_id_intro_;   //! Внутренний ИД заявки исполнителя
+        this->status_description_ = _Src_Request.status_description_; //! Описание статуса
+        this->time_return_vehicle_ = _Src_Request.time_return_vehicle_;    //! Время возвращения ТС
+        this->priority_str_ = _Src_Request.priority_str_;  //! Приоритет (string)
+        this->priority_code_ = _Src_Request.priority_code_;  //! Приоритет (uint)
+        this->request_type_str_ = _Src_Request.request_type_str_;   //! Тип заявки
+        this->employee_full_name_ = _Src_Request.employee_full_name_; //! ФИО сотрудника
+        this->purpose_trip = _Src_Request.purpose_trip;    //! Цель поездки
+        this->category_vehicle_code_ = _Src_Request.category_vehicle_code_;   //! Категория ТС(int)
+        this->gosnomer_vehicle_ = _Src_Request.gosnomer_vehicle_;   //! госномер ТС
+        this->setevoynomer_vehicle_ = _Src_Request.setevoynomer_vehicle_;    //! сетевой номер ТС
+        this->is_intercity_ = _Src_Request.is_intercity_;   //! междугороднаяя перевозка (признак)
+        this->reject_reason_ = _Src_Request.reject_reason_;  //! причина отказа
+        this->reject_reason_str_ = _Src_Request.reject_reason_str_;  //! причина отказа (текст) 
+        this->answer_ = _Src_Request.answer_;  //! все поля с меткой Ответ: (разделены символом ';')
+        this->affiliation_type_vehicle_ = _Src_Request.affiliation_type_vehicle_;   //! Тип транспортного средства по принадлежн
+        this->signature_pixmap_ = _Src_Request.signature_pixmap_;   //! пиктограмма подписи
+        this->signature_fullname_ = _Src_Request.signature_fullname_; //! ФИО подписавшего
+        this->signature_time_ = _Src_Request.signature_time_; //! дата и время подписи
+        this->waybill_number_ = _Src_Request.waybill_number_; //! Номер путевого листа
+        this->OTZ_time_beg_ = _Src_Request.OTZ_time_beg_;   //! ОТЗ. дата и Время начала
+        this->OTZ_time_end_ = _Src_Request.OTZ_time_end_;   //! ОТЗ. дата и Время конца
+        this->OTZ_timework_ = _Src_Request.OTZ_timework_;   //! ОТЗ. Время работы на заказчика
+
+        this->OTZ = _Src_Request.OTZ;  //! все поля с меткой Ответ: (разделены символом ';')
+        this->is_vehicle_not_RZD_ = _Src_Request.is_vehicle_not_RZD_; //! ТС не РЖД (признак)
+        this->MTA_numb_executor_ = _Src_Request.MTA_numb_executor_;  //! Номер МТА от Исполнителя
+        this->area_department_ = _Src_Request.area_department_;    //! Участок структурного подразделения
+        this->violations_str_ = _Src_Request.violations_str_; //! Наименование нарушения при выполнении
+        this->BE_ = _Src_Request.BE_; //! БЕ
+        this->request_time_ = _Src_Request.request_time_;   //! Дата заявки
+        this->request_ID_ = _Src_Request.request_ID_; //! ID заявки на перевозку
+        this->request_status_ = _Src_Request.request_status_; //! Статус заявки на перевозку
+        this->request_status_time_ = _Src_Request.request_status_time_;   //! Дата и время заявки
+        this->work_time_ = _Src_Request.work_time_;   //! Время работы на заказчика
+        this->request_type_ = _Src_Request.request_type_;   //! Тип заявки на перевозку
+        this->voucher_status_ = _Src_Request.voucher_status_; //! Статус проверки талона
+        this->voucher_check_errors_ = _Src_Request.voucher_check_errors_;//! Ошибки при сверке талона
+        this->region_code_ = _Src_Request.region_code_;    //! Регион
+        this->region_str_ = _Src_Request.region_str_; //! Название региона
+        this->motor_depot_str_ = _Src_Request.motor_depot_str_;    //! Наименование автобазы/автоколонны
+        this->is_request_cancel_by_deadline_ = _Src_Request.is_request_cancel_by_deadline_;  //! Заявка отменена с нарушением сроков
+    }
+
+    TransportRequestUnitFull& operator=(const TransportRequestUnitFull & _Src_Request)
+    {
+        if (this == &_Src_Request)
+            return *this;
+
+        this->request_numb_ = _Src_Request.request_numb_;   //! Номер заявки
+        this->request_id_intro_ = _Src_Request.request_id_intro_;   //! Внутренний ИД заявки исполнителя
+        this->status_description_ = _Src_Request.status_description_; //! Описание статуса
+        this->time_return_vehicle_ = _Src_Request.time_return_vehicle_;    //! Время возвращения ТС
+        this->priority_str_ = _Src_Request.priority_str_;  //! Приоритет (string)
+        this->priority_code_ = _Src_Request.priority_code_;  //! Приоритет (uint)
+        this->request_type_str_ = _Src_Request.request_type_str_;   //! Тип заявки
+        this->employee_full_name_ = _Src_Request.employee_full_name_; //! ФИО сотрудника
+        this->purpose_trip = _Src_Request.purpose_trip;    //! Цель поездки
+        this->category_vehicle_code_ = _Src_Request.category_vehicle_code_;   //! Категория ТС(int)
+        this->gosnomer_vehicle_ = _Src_Request.gosnomer_vehicle_;   //! госномер ТС
+        this->setevoynomer_vehicle_ = _Src_Request.setevoynomer_vehicle_;    //! сетевой номер ТС
+        this->is_intercity_ = _Src_Request.is_intercity_;   //! междугороднаяя перевозка (признак)
+        this->reject_reason_ = _Src_Request.reject_reason_;  //! причина отказа
+        this->reject_reason_str_ = _Src_Request.reject_reason_str_;  //! причина отказа (текст) 
+        this->answer_ = _Src_Request.answer_;  //! все поля с меткой Ответ: (разделены символом ';')
+        this->affiliation_type_vehicle_ = _Src_Request.affiliation_type_vehicle_;   //! Тип транспортного средства по принадлежн
+        this->signature_pixmap_ = _Src_Request.signature_pixmap_;   //! пиктограмма подписи
+        this->signature_fullname_ = _Src_Request.signature_fullname_; //! ФИО подписавшего
+        this->signature_time_ = _Src_Request.signature_time_; //! дата и время подписи
+        this->waybill_number_ = _Src_Request.waybill_number_; //! Номер путевого листа
+        this->OTZ_time_beg_ = _Src_Request.OTZ_time_beg_;   //! ОТЗ. дата и Время начала
+        this->OTZ_time_end_ = _Src_Request.OTZ_time_end_;   //! ОТЗ. дата и Время конца
+        this->OTZ_timework_ = _Src_Request.OTZ_timework_;   //! ОТЗ. Время работы на заказчика
+
+        this->OTZ = _Src_Request.OTZ;  //! все поля с меткой Ответ: (разделены символом ';')
+        this->is_vehicle_not_RZD_ = _Src_Request.is_vehicle_not_RZD_; //! ТС не РЖД (признак)
+        this->MTA_numb_executor_ = _Src_Request.MTA_numb_executor_;  //! Номер МТА от Исполнителя
+        this->area_department_ = _Src_Request.area_department_;    //! Участок структурного подразделения
+        this->violations_str_ = _Src_Request.violations_str_; //! Наименование нарушения при выполнении
+        this->BE_ = _Src_Request.BE_; //! БЕ
+        this->request_time_ = _Src_Request.request_time_;   //! Дата заявки
+        this->request_ID_ = _Src_Request.request_ID_; //! ID заявки на перевозку
+        this->request_status_ = _Src_Request.request_status_; //! Статус заявки на перевозку
+        this->request_status_time_ = _Src_Request.request_status_time_;   //! Дата и время заявки
+        this->work_time_ = _Src_Request.work_time_;   //! Время работы на заказчика
+        this->request_type_ = _Src_Request.request_type_;   //! Тип заявки на перевозку
+        this->voucher_status_ = _Src_Request.voucher_status_; //! Статус проверки талона
+        this->voucher_check_errors_ = _Src_Request.voucher_check_errors_;//! Ошибки при сверке талона
+        this->region_code_ = _Src_Request.region_code_;    //! Регион
+        this->region_str_ = _Src_Request.region_str_; //! Название региона
+        this->motor_depot_str_ = _Src_Request.motor_depot_str_;    //! Наименование автобазы/автоколонны
+        this->is_request_cancel_by_deadline_ = _Src_Request.is_request_cancel_by_deadline_;  //! Заявка отменена с нарушением сроков
+        return *this;
     }
     int parseRequest(const std::string & str_str);
 };
